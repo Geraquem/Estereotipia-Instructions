933c57da51c252d6ad5df86e1fbec2f54f9f5318
gers <geraquem@gmail.com>
2025-06-11 18:54:59 +0200

    wip

--- js/toolbar.js
+++ js/toolbar.js
@@ -21,28 +21,28 @@ $(function () {
     $('#enlace-cuadricula').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: cuadricula - 80
+            scrollTop: cuadricula - 50
         }, 600);
     });
 
     $('#enlace-competitivo').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: competitivo - 80
+            scrollTop: competitivo - 50
         }, 600);
     });
 
     $('#enlace-auna').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: auna - 80
+            scrollTop: auna - 50
         }, 600);
     });
 
     $('#enlace-identidades').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: identidades - 80
+            scrollTop: identidades - 50
         }, 600);
     });
 });
